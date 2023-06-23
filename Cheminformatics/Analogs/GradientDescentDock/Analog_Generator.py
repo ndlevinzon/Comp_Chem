a66d52b87c12eb4e7fa41bcdb9d372c960ce73ea
ndlevinzon <59805413+ndlevinzon@users.noreply.github.com>
2023-06-23 12:21:03 -0700

    Update Analog_Generator.py

--- Cheminformatics/Analogs/GradientDescentDock/Analog_Generator.py
+++ Cheminformatics/Analogs/GradientDescentDock/Analog_Generator.py
@@ -4,10 +4,10 @@ import math
 import time
 import re
 import pandas as pd
-from PIL import Image, ImageDraw
+from PIL import Image
 from collections import deque
 from rdkit import Chem
-from rdkit.Chem import AllChem, Draw
+from rdkit.Chem import Draw
 from rdkit.Chem.rdchem import HybridizationType, BondType
 from rdkit.Chem.EnumerateHeterocycles import EnumerateHeterocycles
 
@@ -116,49 +116,45 @@ def BO_stepup(smiles):
 
 
 def BO_stepdown(smiles):
-    """Recursively Changes Bond Order Until All Bonds Are Single"""
-    # Convert the SMILES code to a molecule object
-    mol = Chem.MolFromSmiles(smiles)
-    analogs = []
+    """Decreases Bond Order of Non-Single Bonds"""
 
-    # Adjust valence of the molecule
-    Chem.SanitizeMol(mol, sanitizeOps=Chem.SanitizeFlags.SANITIZE_ADJUSTHS)
+    def convert_to_explicit_bonds(smiles):
+        """Converts SMILES into a representation with explicit bond notation"""
+        mol = Chem.MolFromSmiles(smiles)
+        Chem.Kekulize(mol)  # Ensure explicit bond notation
+        return Chem.MolToSmiles(mol, kekuleSmiles=True)
 
-    # Create a copy of the molecule as an RWMol object
-    rw_mol = Chem.RWMol(mol)
+    def decrease_bond_order_recursive(smiles, bond_indices):
+        """Recursively decreases bond order in the SMILES string"""
+        analogs = []
 
-    # Find indices of carbon-carbon bonds
-    c_c_bond_indices = [bond.GetIdx() for bond in rw_mol.GetBonds() if
-                        bond.GetBeginAtom().GetAtomicNum() == 6 and bond.GetEndAtom().GetAtomicNum() == 6]
+        if len(bond_indices) == 0:
+            return analogs
 
-    def decrease_bond_order_recursive(bond_indices, mol_copy):
-        # Base case: no more bond indices to process
-        if not bond_indices:
-            # Sanitize the molecule to adjust the valence and bonding pattern
-            Chem.SanitizeMol(rw_mol)
-            analogs.append(Chem.RWMol(mol_copy))
-            return
-
-        # Process the first bond index
         bond_idx = bond_indices[0]
-        bond = mol_copy.GetBondWithIdx(bond_idx)
+        bond_symbol = smiles[bond_idx]
 
-        for new_bond_order in range(int(bond.GetBondTypeAsDouble()) - 1, 0, -1):
-            # Create a new molecule copy with decreased bond order
-            mol_copy_new = Chem.RWMol(mol_copy)
-            mol_copy_new.GetBondWithIdx(bond_idx).SetBondType(Chem.BondType(new_bond_order))
-            decrease_bond_order_recursive(bond_indices[1:], mol_copy_new)
+        if bond_symbol == "#":
+            # Decrease triple bond to double bond
+            new_smiles = smiles[:bond_idx] + "=" + smiles[bond_idx + 1:]
+            analogs.append(new_smiles)
+            analogs.extend(decrease_bond_order_recursive(new_smiles, bond_indices[1:]))
 
-        # Process the next bond index without changing the current molecule copy
-        decrease_bond_order_recursive(bond_indices[1:], Chem.RWMol(mol_copy))
+        elif bond_symbol == "=":
+            # Decrease double bond to single bond
+            new_smiles = smiles[:bond_idx] + "-" + smiles[bond_idx + 1:]
+            analogs.append(new_smiles)
+            analogs.extend(decrease_bond_order_recursive(new_smiles, bond_indices[1:]))
 
-    # Start the recursive function to decrease bond order
-    decrease_bond_order_recursive(c_c_bond_indices, rw_mol)
+        return analogs
 
+    explicit_smiles = convert_to_explicit_bonds(smiles)
+    bond_indices = [i for i, symbol in enumerate(explicit_smiles) if symbol in ("#", "=")]
+    analogs_smiles = decrease_bond_order_recursive(explicit_smiles, bond_indices)
     # Remove duplicates and the initial SMILES from the analogs list
-    analogs = [mol for mol in analogs if mol != mol and Chem.MolToSmiles(mol) != smiles]
+    analogs_smiles = [mol for mol in analogs_smiles if mol != smiles]
+    analogs = [Chem.MolFromSmiles(smiles) for smiles in analogs_smiles]
     return analogs
-
 # --------------------------Ring Changes-------------------------- #
 
 def ring_breaker(smiles):
@@ -300,7 +296,7 @@ def ring_maker(smiles):
 # --------------------------Atom Walks-------------------------- #
 
 def walks(smiles, target_num):
-    """Performs Nitrogen Walks On Parent Molecule"""
+    """Performs Walks On Parent Molecule"""
     mol = Chem.MolFromSmiles(smiles)
     analogs = []
     for atom in mol.GetAtoms():
@@ -318,12 +314,6 @@ def walks(smiles, target_num):
     return analogs
 
 
-# Functions to perform specific walking methods
-def nitrogen_walks(smiles): return walks(smiles, target_num=7)
-def oxygen_walks(smiles): return walks(smiles, target_num=8)
-def sulfur_walks(smiles): return walks(smiles, target_num=16)
-
-
 def heterocycle_walks(smiles):
     """Performs Heterocycle Walks On Parent Molecule"""
     mol = Chem.MolFromSmiles(smiles)
@@ -338,12 +328,20 @@ def heterocycle_walks(smiles):
     analogs = [mol for mol in analogs if Chem.MolToSmiles(mol) != smiles]
     return analogs
 
+
+# Functions to perform specific walking methods
+def nitrogen_walks(smiles): return walks(smiles, target_num=7)
+def oxygen_walks(smiles): return walks(smiles, target_num=8)
+def sulfur_walks(smiles): return walks(smiles, target_num=16)
+
 # --------------------------Atom Scans-------------------------- #
 
 def scanning(smiles, atomic_num, n_hs):
     """Scans Parent Molecule Aromatic + Aliphatic Carbons"""
     mol = Chem.AddHs(Chem.MolFromSmiles(smiles))
+    aromatic_atoms = [atom.GetIdx() for atom in mol.GetAtoms() if atom.GetIsAromatic()]
     analogs = []
+
     for atom in mol.GetAtoms():
         if atom.GetSymbol() == 'H':
             n = atom.GetNeighbors()
@@ -356,10 +354,19 @@ def scanning(smiles, atomic_num, n_hs):
             Chem.SanitizeMol(analog)
             analogs.append(analog)
 
+            if atom.GetIsAromatic() and atomic_num == 6:
+                aromatic_atoms_copy = aromatic_atoms.copy()
+                aromatic_atoms_copy.remove(atom.GetIdx())  # Remove the current atom index from the list
+                for position in aromatic_atoms_copy:
+                    a = analog.GetAtomWithIdx(position)
+                    a.SetAtomicNum(atomic_num)
+                    a.SetNumExplicitHs(n_hs)
+                    Chem.SanitizeMol(analog)
+                    analogs.append(analog)
+
     # Remove duplicates and the initial SMILES from the analogs list
     analogs = [mol for mol in analogs if Chem.MolToSmiles(mol) != smiles]
     return analogs
-
 
 # Functions to perform specific scanning methods
 def methyl_scanning(smiles): return scanning(smiles, 6, 3)
@@ -390,32 +397,32 @@ def neutralize_atoms(mol):
 
 def main():
     # Define the analogue methods and their corresponding names
-    analogue_methods = [
-        [trim_extremities, "trim"],
-        [BO_stepup, "increase-bond-order"],
+    analog_methods = [
+        # [trim_extremities, "trim"],
+        # [BO_stepup, "increase-bond-order"],
         [BO_stepdown, "decrease-bond-order"],
-        [ring_breaker, "ring-opening"],
-        [ring_maker, "ring-closure"],
+        # [ring_breaker, "ring-opening"],
+        # [ring_maker, "ring-closure"],
         [nitrogen_walks, "n-walk"],
         [oxygen_walks, "o-walk"],
         [sulfur_walks, "s-walk"],
-        [heterocycle_walks, "heterocycle-walk"],
-        [methyl_scanning, "ch3-scan"],
-        [amine_scanning, "nh2-scan"],
-        [hydroxyl_scanning, "oh-scan"],
-        [thiol_scanning, "sh-scan"],
-        [fluorine_scanning, "f-scan"],
-        [chlorine_scanning, "cl-scan"],
-        [bromine_scanning, "br-scan"],
+        # [heterocycle_walks, "heterocycle-walk"],
+        # [methyl_scanning, "ch3-scan"],
+        # [amine_scanning, "nh2-scan"],
+        # [hydroxyl_scanning, "oh-scan"],
+        # [thiol_scanning, "sh-scan"],
+        # [fluorine_scanning, "f-scan"],
+        # [chlorine_scanning, "cl-scan"],
+        # [bromine_scanning, "br-scan"],
     ]
 
     # Specify the input and output file names
     path = 'C:/Users/ndlev/PycharmProjects/shoichet/analogs/'
-    smiles_input_filename = 'smi-zn-ampc-all.smi'
-    output_file_prefix = "total_benchmark"
+    smiles_input_filename = 'rings.smi'
+    output_file_prefix = "phenol_walks_test"
 
     # Define the list of parent SMILES to take pictures of
-    take_picture = [""]  # Add your desired parent SMILES here
+    take_picture = []  # Add your desired parent SMILES here
 
     # Read the input file and store the smiles and zinc IDs in a DataFrame
     smiles_zinc_input = pd.read_csv(f'{path}{smiles_input_filename}', sep=' ', header=None, names=['Smiles', 'ZincID'])
@@ -430,38 +437,38 @@ def main():
         zinc_id = row['ZincID']
 
         lines_out.append(f"{smiles} {zinc_id}")
-        analogue_key_line = [[smiles, zinc_id], []]
-        all_analogues = []
+        analog_key_line = [[smiles, zinc_id], []]
+        all_analogs = []
 
         # Loop through each analogue method
-        for method in analogue_methods:
-            analogue_key_scan = [method[1], []]
+        for method in analog_methods:
+            analog_key_scan = [method[1], []]
 
             # Generate analogues using the specified method
             for analog in method[0](smiles):
-                analog_smiles = Chem.MolToSmiles(neutralize_atoms(analog), isomericSmiles=True)
+                analog_smiles = Chem.MolToSmiles(analog)
 
-                if analog_smiles not in all_analogues:
+                if analog_smiles not in all_analogs:
                     fakezinc = "fakezinc" + str(analog_count).zfill(8)
                     lines_out.append(f"{analog_smiles} {fakezinc}")
-                    analogue_key_scan[1].append([analog_smiles, fakezinc])
-                    all_analogues.append(analog_smiles)
+                    analog_key_scan[1].append([analog_smiles, fakezinc])
+                    all_analogs.append(analog_smiles)
                     analog_count += 1
                 else:
                     print(analog_smiles)
 
-            analogue_key_line[1].append(analogue_key_scan)
+            analog_key_line[1].append(analog_key_scan)
 
-        analog_key.append(analogue_key_line)
+        analog_key.append(analog_key_line)
 
     # Write the generated smiles and fake zincs to a file
-    output_file = f"{path}{output_file_prefix}-analogues-i{len(smiles_zinc_input)}-o{len(lines_out)}.smi"
+    output_file = f"{path}{output_file_prefix}-analogs-i{len(smiles_zinc_input)}-o{len(lines_out)}.smi"
     with open(output_file, "w") as f2:
         for line in lines_out:
             f2.write(line + "\n")
 
     # Write the key specifying which molecules are analogues of which other molecules and by which analoging processes
-    key_file = f"{path}{output_file_prefix}-key-i{len(smiles_zinc_input)}-o{len(lines_out)}"
+    key_file = f"{path}{output_file_prefix}-key-i{len(smiles_zinc_input)}-o{len(lines_out)}.out"
     with open(key_file, "wb") as fp:
         pickle.dump(analog_key, fp)
 
@@ -479,46 +486,53 @@ def main():
 
     # Check if the "take_picture" list is not empty
     if take_picture:
-        # Generate and save the grid image for each parent SMILES in take_picture
-        for analogue_line in analog_key:
-            parent_smiles = analogue_line[0][0]
-            if parent_smiles in take_picture:
-                for analogue_method in analogue_line[1]:
-                    method = analogue_method[0]
-                    analogue_data = analogue_method[1]
+        # Load the key file specifying which molecules are analogues
+        with open(key_file, "rb") as fp:
+            analog_key = pickle.load(fp)
+            for analog_line in analog_key:
+                parent_smiles = analog_line[0][0]
+                if parent_smiles in take_picture:
+                    for analog_method in analog_line[1]:
 
-                    # Create a list to store analogue images
-                    analogue_images = []
+                        method = analog_method[0]
+                        analog_data = analog_method[1]
 
-                    # Loop through the analogues and add them to the list
-                    for analogue in analogue_data:
-                        analogue_smiles = analogue[0]
-                        fakezinc = analogue[1]
-                        mol = Chem.MolFromSmiles(analogue_smiles)
-                        img = Draw.MolToImage(mol, size=(500, 500))
-                        analogue_images.append(img)
+                        # Create a list to store analog images
+                        analog_images = []
 
-                    # Determine the dimensions of the grid
-                    num_analogues = len(analogue_images)
-                    num_rows = min(10, int(math.ceil(num_analogues / 10)))
-                    num_cols = min(10, num_analogues)
+                        # Loop through the analogues and add them to the list
+                        for analog in analog_data:
+                            analog_smiles = analog[0]
+                            fakezinc = analog[1]
+                            mol = Chem.MolFromSmiles(analog_smiles)
+                            if mol is not None:  # Check if the molecule object is valid
+                                img = Draw.MolToImage(mol, size=(50, 50))
+                                analog_images.append(img)
+                            else:
+                                print(f"Invalid molecule: {analog_smiles}")
 
-                    # Create the grid image
-                    grid_image = Image.new('RGB', (num_cols * 500, num_rows * 500))
+                        # Determine the dimensions of the grid
+                        num_analogs = len(analog_images)
+                        num_rows = min(10, int(math.ceil(num_analogs / 10)))
+                        num_cols = min(10, num_analogs)
 
-                    # Loop through the analogues and paste them into the grid
-                    for i, analogue_image in enumerate(analogue_images):
-                        col_idx = i % 10
-                        row_idx = i // 10
-                        grid_image.paste(analogue_image, (col_idx * 500, row_idx * 500))
+                        # Create the grid image
+                        grid_image = Image.new('RGB', (num_cols * 500, num_rows * 500))
 
-                    # Add the parent SMILES and method label to the top of the image
-                    draw = ImageDraw.Draw(grid_image)
-                    label = f"Parent SMILES: {parent_smiles}\nMethod: {method}"
-                    draw.text((10, 10), label, fill="white")
+                        # Loop through the analogs and paste them into the grid
+                        for i, analog_image in enumerate(analog_images):
+                            col_idx = i % num_cols
+                            row_idx = i // num_cols
+                            offset_x = col_idx * 500
+                            offset_y = row_idx * 500
+                            resized_analog_image = analog_image.resize((400, 400))  # Resize the analog image if needed
+                            grid_image.paste(resized_analog_image, (offset_x, offset_y))
 
-                    # Save the grid image as a PNG file
-                    grid_image.save(f"{path}{output_file_prefix}-{parent_smiles}-{method}.png")
+                        # Save the grid image as a PNG file
+                        try:
+                            grid_image.save(f"{path}{parent_smiles}-{method}.png")
+                        except Exception as e:
+                            print(f"Error saving the grid image: {e}")
 
 
 if __name__ == '__main__':
