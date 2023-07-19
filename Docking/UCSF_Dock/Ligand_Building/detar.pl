2c28a9837a705967898fb3b88156123722d6bac5
ndlevinzon <59805413+ndlevinzon@users.noreply.github.com>
2023-07-19 11:05:44 -0700

    Update detar.pl

--- Docking/UCSF_Dock/Ligand_Building/detar.pl
+++ Docking/UCSF_Dock/Ligand_Building/detar.pl
@@ -12,7 +12,7 @@ my @tarballs = glob('*.tar.gz');
 
 # Loop through each tarball and extract its contents
 foreach my $tarball (@tarballs) {
-    # Run the 'tar xf' command for each tarball
+    # Run the 'tar xzvf' command for each tarball
     system("tar xf $tarball");
 }
 
