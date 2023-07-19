b2a4bbdae0be5952cdb1a99ccd2e3e21e2f78338
ndlevinzon <59805413+ndlevinzon@users.noreply.github.com>
2023-07-19 10:57:15 -0700

    Update detar.pl

--- Docking/UCSF_Dock/Ligand_Building/detar.pl
+++ Docking/UCSF_Dock/Ligand_Building/detar.pl
@@ -3,6 +3,10 @@
 use strict;
 use warnings;
 
+# Get the current directory
+my $current_directory = `pwd`;
+chomp($current_directory);
+
 # Get a list of all .tar.gz files in the current directory
 my @tarballs = glob('*.tar.gz');
 
@@ -11,3 +15,12 @@ foreach my $tarball (@tarballs) {
     # Run the 'tar xf' command for each tarball
     system("tar xf $tarball");
 }
+
+# Get the parent directory (repack_directory is located in the directory directly above the current directory)
+my $repack_directory = `cd .. && pwd`;
+chomp($repack_directory);
+$repack_directory .= '/main/repack';
+
+# After extracting, run the 'make_tarballs.bash' script with the specified arguments
+my $make_tarballs_script = 'make_tarballs.bash';
+system("bash $make_tarballs_script $current_directory $repack_directory");
