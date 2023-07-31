b84cb990f79c7f24d962bf04531f647c0158988e
ndlevinzon <59805413+ndlevinzon@users.noreply.github.com>
2023-07-31 13:51:23 -0700

    Update Analog_Generator_Dev.py

--- Cheminformatics/Analogs/GradientDescentDock/Analog_Generator_Dev.py
+++ Cheminformatics/Analogs/GradientDescentDock/Analog_Generator_Dev.py
@@ -1,21 +1,14 @@
-# Jonathan Borowsky and Nathan Levinzon, UCSF 2023
-import argparse
-import pickle
-import math
+# Nathan Levinzon, Jonathan Borowsky, and Olivier Mailhot  UCSF 2023
+# Non-refactored Version
 import time
 import re
 import pandas as pd
-from PIL import Image
+from collections import defaultdict
 from collections import deque
 from rdkit import Chem
-from rdkit.Chem import Draw, Descriptors
-from rdkit.Chem.rdchem import HybridizationType, BondType, ChiralType, BondStereo, AtomValenceException
+from rdkit.Chem import Descriptors
+from rdkit.Chem.rdchem import HybridizationType, BondType
 from rdkit.Chem.EnumerateHeterocycles import EnumerateHeterocycles
-from rdkit.Chem.EnumerateStereoisomers import EnumerateStereoisomers
-
-# Get the current time before running the code
-start_time = time.time()
-print(f"Starting Time: {start_time}")
 
 # --------------------------Extremity Trimmer-------------------------- #
 
@@ -33,7 +26,7 @@ def trim_extremities(smiles):
 
     # Exit the function if the molecular weight is less than or equal to 500 Da
     if mw <= 500:
-        return
+        return []
     analogs = []
 
     # Adjust valence of the molecule
@@ -108,6 +101,7 @@ def BO_stepup(smiles):
             # Increase bond order from single to double
             try:
                 mol_copy_new.GetBondWithIdx(bond_idx).SetBondType(BondType.DOUBLE)
+                Chem.AdjustBondStereo(mol_copy_new, bond_idx)  # Adjust bond stereochemistry
                 analogs.append(Chem.RWMol(mol_copy_new))
             except:
                 print("Error: Could not make Double Bond")
@@ -115,6 +109,7 @@ def BO_stepup(smiles):
             # Increase bond order from double to triple
             try:
                 mol_copy_new.GetBondWithIdx(bond_idx).SetBondType(BondType.TRIPLE)
+                Chem.AdjustBondStereo(mol_copy_new, bond_idx)  # Adjust bond stereochemistry
                 analogs.append(Chem.RWMol(mol_copy_new))
             except:
                 print("Error: Could not make Triple Bond")
@@ -198,24 +193,23 @@ def ring_breaker(smiles):
             # Create a copy of the molecule
             mol_copy = Chem.RWMol(mol)
 
-            # Remove the bond between atom1 and atom2
+            # Remove the bond between atom1 and atom2 if it exists
             bond = mol_copy.GetBondBetweenAtoms(atom1, atom2)
             if bond is not None:
                 mol_copy.RemoveBond(atom1, atom2)
 
-                # Add new atoms and bonds based on modified adjacency matrix
+                # Add new atoms and bonds based on the modified adjacency matrix
                 adjacency_matrix = Chem.GetAdjacencyMatrix(mol_copy)
                 for j in range(len(adjacency_matrix)):
                     for k in range(j + 1, len(adjacency_matrix)):
                         if adjacency_matrix[j][k] and not mol_copy.GetBondBetweenAtoms(j, k):
                             mol_copy.AddBond(j, k, BondType.SINGLE)
 
-                # Update the molecule properties and sanitize
+                # Update the molecule properties and sanitize with kekulization
                 mol_copy.UpdatePropertyCache(strict=False)
                 try:
-                    Chem.SanitizeMol(mol_copy,
-                                     sanitizeOps=Chem.SanitizeFlags.SANITIZE_ALL ^ Chem.SanitizeFlags.SANITIZE_PROPERTIES)
-                    Chem.Kekulize(mol_copy)
+                    Chem.SanitizeMol(mol_copy, sanitizeOps=Chem.SanitizeFlags.SANITIZE_ALL ^ Chem.SanitizeFlags.SANITIZE_PROPERTIES)
+                    Chem.SanitizeMol(mol_copy, sanitizeOps=Chem.SanitizeFlags.SANITIZE_KEKULIZE)
 
                     # Process sanitized analogs
                     sanitized_analogs = [mol_copy]
@@ -229,14 +223,16 @@ def ring_breaker(smiles):
                         sanitized_analog = Chem.MolFromSmiles(smiles)
                         if sanitized_analog is not None:
                             analogs.append(sanitized_analog)
-                except AtomValenceException:
-                    # Skip if valence adjustment fails during sanitization
+                except (Chem.AtomValenceException, Chem.KekulizeException):
+                    # Skip if valence adjustment or kekulization fails during sanitization
                     continue
 
     # Remove duplicates and the initial SMILES from the analogs list
     analogs = [mol for mol in analogs if Chem.MolToSmiles(mol) != smiles]
 
     return analogs
+
+
 
 def ring_maker(smiles):
     """Enumerates Terminal -CH3, Finds Paths Of Length (4, 5) And Forms Rings With sp3 Carbons On Path"""
@@ -334,9 +330,14 @@ def walks(smiles, target_num):
             a_neighbors = [n for n in a.GetNeighbors()]
             if len(a_neighbors) == 2:
                 a.SetAtomicNum(target_num)
-                analog_mol = Chem.MolFromSmiles(Chem.MolToSmiles(analog, isomericSmiles=True))
-                if analog_mol is not None:
-                    analogs.append(analog_mol)
+                try:
+                    analog_mol = Chem.MolFromSmiles(Chem.MolToSmiles(analog, isomericSmiles=True))
+                    if analog_mol is not None:
+                        analogs.append(analog_mol)
+                except Chem.KekulizeException:
+                    # Skip the molecule if kekulization fails
+                    print(f"Kekulization error for molecule: {Chem.MolToSmiles(analog)}")
+                    continue
 
     # Remove duplicates and the initial SMILES from the analogs list
     analogs = [mol for mol in analogs if mol is not None and Chem.MolToSmiles(mol) != smiles]
@@ -352,14 +353,18 @@ def heterocycle_walks(smiles):
 
     # Enumerate heterocycles and append RWMol objects to analogs list
     for mol in EnumerateHeterocycles(mol, depth=1):
-        analog = Chem.RWMol(mol)
-        analogs.append(analog)
+        try:
+            analog = Chem.RWMol(mol)
+            analogs.append(analog)
+        except Chem.KekulizeException:
+            # Skip the molecule if kekulization fails
+            print(f"Kekulization error for molecule: {Chem.MolToSmiles(mol)}")
+            continue
 
     # Remove duplicates and the initial SMILES from the analogs list
     analogs = [mol for mol in analogs if Chem.MolToSmiles(mol) != smiles]
 
     return analogs
-
 
 # Functions to perform specific walking methods
 def nitrogen_walks(smiles): return walks(smiles, target_num=7)
@@ -432,6 +437,7 @@ def thiol_scanning(smiles): return scanning(smiles, ['S'])
 def fluorine_scanning(smiles): return scanning(smiles, ['F'])
 def chlorine_scanning(smiles): return scanning(smiles, ['Cl'])
 def bromine_scanning(smiles): return scanning(smiles, ['Br'])
+def iodine_scanning(smiles): return scanning(smiles, ['I'])
 
 def bioisosters_scanning(smiles):
     fragments = [
@@ -457,35 +463,54 @@ def bioisosters_scanning(smiles):
     ]
     return scanning(smiles, fragments)
 
-# --------------------------Atom Neutralization-------------------------- #
+# --------------------------Stereoisomers--------------------------#
 
-def neutralize_atoms(mol):
-    pattern = Chem.MolFromSmarts("[+1!h0!$([*]~[-1,-2,-3,-4]),-1!$([*]~[+1,+2,+3,+4])]")
-    at_matches = mol.GetSubstructMatches(pattern)
-    at_matches_list = [y[0] for y in at_matches]
-    if len(at_matches_list) > 0:
-        for at_idx in at_matches_list:
-            atom = mol.GetAtomWithIdx(at_idx)
-            chg = atom.GetFormalCharge()
-            hcount = atom.GetTotalNumHs()
-            atom.SetFormalCharge(0)
-            atom.SetNumExplicitHs(hcount - chg)
-            atom.UpdatePropertyCache()
-    return mol
+def enumerate_stereoisomers(mol):
+    """Enumerate stereoisomers of the input RWMol object.
 
+    Parameters:
+        mol (RWMol): The input RWMol object.
 
-# --------------------------I/O--------------------------#
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Generate analogs from a .smi file.")
-    parser.add_argument("-i", "--input", type=str, required=True, help="Input SMILES file name.")
-    parser.add_argument("-o", "--output", type=str, required=True, help="Output file prefix.")
-    return parser.parse_args()
+    Returns:
+        List[RWMol] or None: A list of RWMol objects representing each unique stereoisomer,
+                             or None if there are no stereoisomers to enumerate.
+    """
+    if mol is None:
+        raise ValueError("Invalid input RWMol object.")
+
+    # Find all stereocenters in the molecule
+    stereo_centers = [atom.GetIdx() for atom in mol.GetAtoms() if atom.HasProp('_ChiralityPossible')]
+
+    # If there are no stereocenters, return None
+    if not stereo_centers:
+        return None
+
+    # Enumerate all possible combinations of stereo configurations
+    num_stereo_centers = len(stereo_centers)
+    num_combinations = 2 ** num_stereo_centers
+
+    stereoisomers = []
+    for i in range(num_combinations):
+        isomer = Chem.RWMol(mol)
+        for j in range(num_stereo_centers):
+            atom_idx = stereo_centers[j]
+            # Get the original atom chirality
+            original_chirality = mol.GetAtomWithIdx(atom_idx).GetChiralTag()
+            # Invert the chirality for this isomer based on the combination
+            isomer.GetAtomWithIdx(atom_idx).InvertChirality()
+            # Check if this isomer already exists in the list
+            if isomer not in stereoisomers:
+                stereoisomers.append(isomer)
+
+    return stereoisomers
 
 # --------------------------Switchboard--------------------------#
 
 def main():
-    # Get command-line arguments
-    args = parse_arguments()
+
+    # Get the current time before running the code
+    start_time = time.time()
+    print(f"Starting Time: {start_time}")
 
     # Define the analogue methods and their corresponding names
     analog_methods = [
@@ -505,76 +530,77 @@ def main():
         [fluorine_scanning, "f-scan"],
         [chlorine_scanning, "cl-scan"],
         [bromine_scanning, "br-scan"],
+        [iodine_scanning, "i-scan"],
         [bioisosters_scanning, "bioisoster-scan"],
     ]
 
     # Specify the input and output file names
-    path = os.getcwd()
-    smi_input_filename = args.input
-    output_file_prefix = args.output
+    path = 'C:/Users/ndlev/PycharmProjects/shoichet/analogs/'
+    smiles_input_filename = 'jk_ampc_ligands.smi'
+    output_file_prefix = "jk_test"
 
     # Read the input file and store the smiles and zinc IDs in a DataFrame
-    smiles_zinc_input = pd.read_csv(os.path.join(path, smi_input_filename),
-                                    sep=' ', header=None, names=['Smiles', 'ZincID'])
+    smiles_zinc_input = pd.read_csv(f'{path}{smiles_input_filename}', sep=' ', header=None, names=['Smiles', 'ZincID'])
 
-    analog_key = []
-    lines_out = []
-    analog_count = 0
+    # Create a dictionary to store the analogs for each input SMILES code
+    all_analogs_dict = defaultdict(set)  # Use sets for analogs
 
     # Loop through each input molecule
+    total_analogs_count = 0
     for _, row in smiles_zinc_input.iterrows():
         smiles = row['Smiles']
         zinc_id = row['ZincID']
 
-        lines_out.append(f"{smiles} {zinc_id}")
-        analog_key_line = [[smiles, zinc_id], []]
-        all_analogs = []
-
-        # Loop through each analogue method
+        # Loop through each analogue method for the current input molecule
         for method in analog_methods:
-            analog_key_scan = [method[1], []]
-
             # Generate analogues using the specified method
             try:
                 for analog in method[0](smiles):
                     try:
-                        analog_smiles = Chem.MolToSmiles(analog, isomericSmiles=True)
-                        if not analog_smiles:
-                            raise ValueError(f"Invalid SMILES: {analog_smiles}")
-                        if analog_smiles not in all_analogs:
-                            fakezinc = str(zinc_id) + "_analog"+ str(analog_count).zfill(4)
-                            lines_out.append(f"{analog_smiles} {fakezinc}")
-                            analog_key_scan[1].append([analog_smiles, fakezinc])
-                            all_analogs.append(analog_smiles)
-                            analog_count += 1
+                        # Enumerate stereoisomers for the current analog
+                        stereoisomers = enumerate_stereoisomers(analog)
+                        if stereoisomers is None:
+                            analog_smiles = Chem.MolToSmiles(analog, isomericSmiles=True)
+                            if not analog_smiles:
+                                raise ValueError(f"Invalid SMILES: {analog_smiles}")
+                            # Add analog to the set
+                            all_analogs_dict[smiles].add(analog_smiles)
+                            total_analogs_count += 1
+                            # Print analog SMILES and create fakezinc inside the loop
+                            fakezinc = zinc_id + "_analog" + str(total_analogs_count).zfill(4)
+                            print(analog_smiles, fakezinc)
                         else:
-                            continue
+                            for isomer in stereoisomers:
+                                analog_smiles = Chem.MolToSmiles(isomer, isomericSmiles=True)
+                                if not analog_smiles:
+                                    raise ValueError(f"Invalid SMILES: {analog_smiles}")
+                                # Add analog to the set
+                                all_analogs_dict[smiles].add(analog_smiles)
+                                total_analogs_count += 1
+                                # Print analog SMILES and create fakezinc inside the loop
+                                fakezinc = zinc_id + "_analog"
+                                print(analog_smiles, fakezinc)
                     except ValueError as e:
                         print(str(e))  # Skip the entry and print the error message
                         continue  # Skip to the next iteration
-                    print(analog_smiles)
-
-                analog_key_line[1].append(analog_key_scan)
-
             except Exception as e:
                 print(f"Error generating analogs: {str(e)}")
                 continue
 
-        analog_key.append(analog_key_line)
-
     # Write the generated smiles and fake zincs to a file
-    output_file = f"{path}/{output_file_prefix}-analogs-i{len(smiles_zinc_input)}-o{len(lines_out)}.smi"
+    output_file = f"{path}{output_file_prefix}-analogs-i{len(smiles_zinc_input)}-o{(sum(len(analogs) for _, analogs in all_analogs_dict.items()))}.smi"
     with open(output_file, "w") as f2:
-        for line in lines_out:
-            f2.write(line + "\n")
-
-    # Write the key specifying which molecules are analogues of which other molecules and by which analoging processes
-    key_file = f"{path}/{output_file_prefix}-key-i{len(smiles_zinc_input)}-o{len(lines_out)}.out"
-    with open(key_file, "wb") as fp:
-        pickle.dump(analog_key, fp)
+        for _, row in smiles_zinc_input.iterrows():
+            smiles = row['Smiles']
+            zinc_id = row['ZincID']
+            f2.write(f"{smiles} {zinc_id}\n")
+            for analog_smiles in all_analogs_dict[smiles]:
+                total_analogs_count += 1
+                fakezinc = zinc_id + "_analog" + str(total_analogs_count).zfill(4)
+                f2.write(f"{analog_smiles} {fakezinc}\n")
 
     # Calculate the number of analogs generated
-    print(f"Molecules: {len(lines_out)} molecules")
+    print(f"Molecules: {total_analogs_count} molecules")
 
     # Calculate the elapsed time
     end_time = time.time()
@@ -582,61 +608,8 @@ def main():
     print(f"Runtime: {elapsed_time} seconds")
 
     # Calculate molecules generated per second
-    benchmark = float(len(lines_out)) / elapsed_time
+    benchmark = float(sum(len(analogs) for _, analogs in all_analogs_dict.items())) / elapsed_time
     print(f"Benchmark: {benchmark} molecules/second")
-
-    # Define the list of parent SMILES to take pictures of
-    take_picture = []  # Add your desired parent SMILES here
-
-    # Check if the "take_picture" list is not empty
-    if take_picture:
-        # Load the key file specifying which molecules are analogues
-        with open(key_file, "rb") as fp:
-            analog_key = pickle.load(fp)
-            for analog_line in analog_key:
-                parent_smiles = analog_line[0][0]
-                if parent_smiles in take_picture:
-                    for analog_method in analog_line[1]:
-
-                        method = analog_method[0]
-                        analog_data = analog_method[1]
-
-                        # Create a list to store analog images
-                        analog_images = []
-
-                        # Loop through the analogues and add them to the list
-                        for analog in analog_data:
-                            analog_smiles = analog[0]
-                            fakezinc = analog[1]
-                            mol = Chem.MolFromSmiles(analog_smiles)
-                            if mol is not None:  # Check if the molecule object is valid
-                                img = Draw.MolToImage(mol, size=(50, 50))
-                                analog_images.append(img)
-                            else:
-                                print(f"Invalid molecule: {analog_smiles}")
-
-                        # Determine the dimensions of the grid
-                        num_analogs = len(analog_images)
-                        num_rows = min(10, int(math.ceil(num_analogs / 10)))
-                        num_cols = min(10, num_analogs)
-
-                        # Create the grid image
-                        grid_image = Image.new('RGB', (num_cols * 500, num_rows * 500))
-
-                        # Loop through the analogs and paste them into the grid
-                        for i, analog_image in enumerate(analog_images):
-                            col_idx = i % num_cols
-                            row_idx = i // num_cols
-                            offset_x = col_idx * 500
-                            offset_y = row_idx * 500
-                            resized_analog_image = analog_image.resize((400, 400))  # Resize the analog image if needed
-                            grid_image.paste(resized_analog_image, (offset_x, offset_y))
-
-                        # Save the grid image as a PNG file
-                        try:
-                            grid_image.save(f"{path}/{parent_smiles}-{method}.png")
-                        except Exception as e:
-                            print(f"Error saving the grid image: {e}")
 
 
 if __name__ == '__main__':
